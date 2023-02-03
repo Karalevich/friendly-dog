70dbe42e4328478fab4635d6f240a16a58bccd75
AndreyKorolevich <korolevich1994@mail.ru>
2023-02-02 22:34:12 -0800

    Add player sprite animation

--- src/app/source.ts
+++ src/app/source.ts
@@ -1,16 +1,42 @@
 import shadowDog from '../img/shadow_dog.png'
-
-const canvas = document.getElementById('canvas1') as HTMLCanvasElement
-const context =  canvas?.getContext('2d') as CanvasRenderingContext2D
-const CANVAS_WIDTH = canvas.width = 600
-const CANVAS_HEIGHT = canvas.height = 600
+import {
+  CANVAS_HEIGHT,
+  CANVAS_WIDTH,
+  context, PLAYER_STATES,
+  SPRITE_HEIGHT,
+  SPRITE_WIDTH, STAGGER_FRAMES
+} from '../constants/canvas-const'
+import { AnimationNameType, AnimationType } from '../types/types'
 
 const playerImg = new Image()
 playerImg.src = shadowDog
 
+let gameFrame = 0
+let playerState = 'idle'
+
+const playerSpriteAnimation: AnimationType = {}
+PLAYER_STATES.forEach((state, index) => {
+  let frames: AnimationNameType = {
+    loc: []
+  }
+  for (let i = 0; i < state.frames; i++) {
+    let positionX = i * SPRITE_WIDTH
+    let positionY = index * SPRITE_HEIGHT
+    frames.loc.push({ x: positionX, y: positionY })
+  }
+  playerSpriteAnimation[state.name] = frames
+})
+
+console.log(playerSpriteAnimation)
+
 const animate = (): void => {
   context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-  context.drawImage(playerImg, 0, 0)
+  let position = Math.floor(gameFrame / STAGGER_FRAMES) % playerSpriteAnimation[playerState].loc.length
+  let frameX = position * SPRITE_WIDTH
+  let frameY = playerSpriteAnimation[playerState].loc[position].y
+  context.drawImage(playerImg, frameX, frameY, SPRITE_WIDTH, SPRITE_HEIGHT, 0, 0, SPRITE_WIDTH, SPRITE_HEIGHT)
+
+  gameFrame++
   requestAnimationFrame(animate)
 }
 
---
+++ src/constants/canvas-const.ts
@@ -0,0 +1,58 @@
+import { StateType } from '../types/types'
+
+export const PLAYER_IMAGE_WIDTH = 6876
+export const PLAYER_IMAGE_HEIGHT = 5230
+export const PLAYER_IMAGE_COLUMNS = 12
+export const PLAYER_IMAGE_ROWS = 10
+export const STAGGER_FRAMES = 5
+
+const canvas = document.getElementById('canvas1') as HTMLCanvasElement
+export const context = canvas?.getContext('2d') as CanvasRenderingContext2D
+export const CANVAS_WIDTH = canvas.width = 600
+export const CANVAS_HEIGHT = canvas.height = 600
+
+export const SPRITE_WIDTH = 575
+export const SPRITE_HEIGHT = PLAYER_IMAGE_HEIGHT / PLAYER_IMAGE_ROWS
+
+export const PLAYER_STATES: Array<StateType> = [
+  {
+    name: 'idle',
+    frames: 7
+  },
+  {
+    name: 'jump',
+    frames: 7
+  },
+  {
+    name: 'fall',
+    frames: 9
+  },
+  {
+    name: 'run',
+    frames: 9
+  },
+  {
+    name: 'dizzy',
+    frames: 11
+  },
+  {
+    name: 'sit',
+    frames: 5
+  },
+  {
+    name: 'roll',
+    frames: 7
+  },
+  {
+    name: 'bite',
+    frames: 7
+  },
+  {
+    name: 'ko',
+    frames: 12
+  },
+  {
+    name: 'getHit',
+    frames: 4
+  }
+]
